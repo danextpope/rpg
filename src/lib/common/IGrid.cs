00f263aa82e4b41ad27ab8adad527d005e93dad8
Ari Jordon <Ari.Jordon@prometric.com>
2018-07-12 07:49:15 -0400

    Added a generic grid interface

---
+++ src/lib/common/IGrid.cs
@@ -0,0 +1,15 @@
+﻿namespace FourZoas.RPG.Common
+{
+    using System.Collections.Generic;
+
+    public interface IGrid<T>
+    {
+        int Height { get; }
+
+        int Width { get; }
+
+        T this[int x, int y] { get; set; }
+
+        IEnumerable<T> Neighbors(int x, int y);
+    }
+}
