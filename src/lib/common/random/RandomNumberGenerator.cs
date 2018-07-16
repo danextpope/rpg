f5465c1c119697d43984e8628bb42156ad3f8452
Ari Jordon <Ari.Jordon@prometric.com>
2018-07-16 11:08:50 -0400

    Random number generator code.

---
+++ src/lib/common/random/RandomNumberGenerator.cs
@@ -0,0 +1,56 @@
+﻿namespace FourZoas.RPG.Common
+{
+    using System;
+
+    /// <summary>A random number generator wrapper around <see cref="Random"/>.</summary>
+    /// <seealso cref="IRandom"/>
+    public class RandomNumberGenerator : IRandom
+    {
+        private Random random;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomNumberGenerator"/> class with a random
+        /// seed value.
+        /// </summary>
+        public RandomNumberGenerator() : this(new Random().Next())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomNumberGenerator"/> class with the
+        /// given seed value.
+        /// </summary>
+        /// <param name="seed">The seed.</param>
+        public RandomNumberGenerator(int seed)
+        {
+            Seed = seed;
+            random = new Random(seed);
+        }
+
+        /// <summary>Gets the random number seed.</summary>
+        /// <value>The random number seed.</value>
+        public int Seed { get; }
+
+        /// <summary>Gets a random interger from 0 to <see cref="int.MaxValue"/>.</summary>
+        /// <returns>A random interger from 0 to <see cref="int.MaxValue"/>.</returns>
+        public int Get() => random.Next();
+
+        /// <summary>Gets a random integer from 0 to <c>maxValue</c>.</summary>
+        /// <param name="maxValue">The exclusive maximum value.</param>
+        /// <returns>A random integer from 0 to <c>maxValue</c>.</returns>
+        public int Get(int maxValue) => random.Next(maxValue);
+
+        /// <summary>Gets a random number from <c>minValue</c> to <c>maxValue</c>.</summary>
+        /// <param name="minValue">The inclusive minimum value.</param>
+        /// <param name="maxValue">The exclusive maximum value.</param>
+        /// <returns>A random number from <c>minValue</c> to <c>maxValue</c>.</returns>
+        public int Get(int minValue, int maxValue) => random.Next(minValue, maxValue);
+
+        /// <summary>Gets a random double value from <c>0</c> to <c>1</c>.</summary>
+        /// <returns>A random double value from <c>0</c> to <c>1</c>.</returns>
+        public double GetDouble() => random.NextDouble();
+
+        /// <summary>Resets this instance.</summary>
+        public void Reset() => random = new Random(Seed);
+    }
+}
