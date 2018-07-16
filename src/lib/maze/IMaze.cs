942fabdbd9e65fe2b1780083756aff7e4ac4edff
Ari Jordon <Ari.Jordon@prometric.com>
2018-07-16 09:53:16 -0400

    Added skeleton for maze interface

---
+++ src/lib/maze/IMaze.cs
@@ -0,0 +1,14 @@
+﻿namespace FourZoas.RPG.Maze
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// A generic maze interface.
+    /// </summary>
+    /// <typeparam name="T">The type of data stored in the maze.</typeparam>
+    public interface IMaze<T>
+    {
+    }
+}
